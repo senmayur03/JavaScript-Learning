88e9b42a8a02c8ec5658a45bdbfa31a690c57643
TheTacticalNuke <60157364+senmayur03@users.noreply.github.com>
2022-01-20 18:25:52 +0000

    Update conditions.js

---
+++ conditions.js
@@ -0,0 +1,20 @@
+console.clear()
+
+if (5 > 7) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+var day = "Tuesday"
+
+switch(day) {
+    case 'Monday':
+        console.log("Monday")
+        break;
+    default:
+        console.log("Not Monday")
+        break;
+}
+
