e694ef933a1525a529e679bd5bdd1db8b2b6e2ed
TheTacticalNuke <60157364+senmayur03@users.noreply.github.com>
2022-01-20 13:16:55 +0000

    datatypes finished, conditions started

--- datatypes.js
+++ datatypes.js
@@ -16,9 +16,18 @@ var num5 = 10
 // console.log(num5.toFixed(3))
 
 var myString = "Hello \"Big World\" World"
-var testString = "This is a test string, This is another string"
+var testString = "    This is a test string, This is another string"
 
-console.log(testString.length)
-console.log(testString.indexOf('test'))
-console.log(testString.indexOf('string'))
-console.log(testString.slice(3, 10))
+// console.log(testString.length)
+// console.log(testString.indexOf('test'))
+// console.log(testString.indexOf('string'))
+// console.log(testString.slice(5))
+// console.log(testString.substr(5, 10))
+
+console.log(testString.concat(myString, myString))
+console.log(testString.trim())
+console.log(testString.charAt(7))
+console.log(testString.split(' '))
+
+var undefinedVar
+console.log(undefinedVar)
