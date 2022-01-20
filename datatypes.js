00e8f67e7da65e686988986bb3e77980034263ec
TheTacticalNuke <60157364+senmayur03@users.noreply.github.com>
2022-01-20 12:21:09 +0000

    Update datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+console.clear()
+var num1 = 2;
+var num2 = 4;
+var num3 = 6.5;
+
+var num4 = num1 + num2 + num3;
+
+// console.log(num4.toString() + " " + num3)
+// console.log(typeof (num2))
+// console.log(typeof (NaN))
+
+// console.log(parseFloat("10.4"))
+
+var num5 = 10
+
+// console.log(num5.toFixed(3))
+
+var myString = "Hello \"Big World\" World"
+var testString = "This is a test string, This is another string"
+
+console.log(testString.length)
+console.log(testString.indexOf('test'))
+console.log(testString.indexOf('string'))
+console.log(testString.slice(3, 10))
