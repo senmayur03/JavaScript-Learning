e3da2a94a31bb07ac5a3c510c5264e056addfee8
TheTacticalNuke <60157364+senmayur03@users.noreply.github.com>
2022-01-23 14:56:47 +0000

    Moved onto learning Type Script

--- learning.js
+++ learning.js
@@ -73,44 +73,46 @@
 // car.seating = 2
 // console.log(car.hasOwnProperty("seating"))
 
-var showroom = [
-    {
-        "make": "Honda",
-        "model": "NSX"
-    },
-    {
-        "make": "Ferrari",
-        "model": "599"
-    },
-    {
-        "make": "Lexus",
-        "model": "LFA"
-    },
-    {
-        "make": "Bugatti",
-        "model": "Chiron"
-    }
-]
+// var showroom = [
+//     {
+//         "make": "Honda",
+//         "model": "NSX"
+//     },
+//     {
+//         "make": "Ferrari",
+//         "model": "599"
+//     },
+//     {
+//         "make": "Lexus",
+//         "model": "LFA"
+//     },
+//     {
+//         "make": "Bugatti",
+//         "model": "Chiron"
+//     }
+// ]
 
-console.log(showroom[0]["make"])
-console.log(showroom[1]["model"])
+// console.log(showroom[0]["make"])
+// console.log(showroom[1]["model"])
 
-function lookUpCars(make, model, room) {
-    var carFound = false
-    for (i = 0; i < room.length; i++){
-        if ((room[i]["make"] == make) && (room[i]["model"] == model)){
-            carFound = true
-        }
-    }
-    if (carFound == true) {
-        console.log("Car does exist in this showroom.")
-    }
-    else {
-        console.log("Car does not exist in this showroom")
-    }
-}
+// function lookUpCars(make, model, room) {
+//     var carFound = false
+//     for (i = 0; i < room.length; i++){
+//         if ((room[i]["make"] == make) && (room[i]["model"] == model)){
+//             carFound = true
+//         }
+//     }
+//     if (carFound == true) {
+//         console.log("Car does exist in this showroom.")
+//     }
+//     else {
+//         console.log("Car does not exist in this showroom")
+//     }
+// }
 
-lookUpCars("Honda", "NSX", showroom)
-lookUpCars("Bugatti", "Chiron", showroom)
-lookUpCars("Bugatti", "Veyron", showroom)
-lookUpCars("Ferrari", "FXX", showroom)
+// lookUpCars("Honda", "NSX", showroom)
+// lookUpCars("Bugatti", "Chiron", showroom)
+// lookUpCars("Bugatti", "Veyron", showroom)
+// lookUpCars("Ferrari", "FXX", showroom)
+
+console.log(parseInt("10101101", 2))
