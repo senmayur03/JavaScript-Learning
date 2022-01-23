1ba82b2f905217cee395f98f8720cbf17026d981
TheTacticalNuke <60157364+senmayur03@users.noreply.github.com>
2022-01-23 12:34:14 +0000

    Update learning.js

--- learning.js
+++ learning.js
@@ -47,3 +47,70 @@
 //         console.log(names[offsetDiff])
 //     }
 // }
+
+// var car = {
+//     "name": "Honda",
+//     "model": "NSX",
+//     "year": 2000,
+// }
+
+// console.log(car.year)
+// console.log(car["name"])
+
+// var model = "model"
+// console.log(car[model])
+
+// car.year = 2015
+// console.log(car.year)
+
+// car.speed = "300km/h"
+// console.log(car.speed)
+
+// delete car.year
+// console.log(car)
+ 
+// console.log(car.hasOwnProperty("seating"))
+// car.seating = 2
+// console.log(car.hasOwnProperty("seating"))
+
+var showroom = [
+    {
+        "make": "Honda",
+        "model": "NSX"
+    },
+    {
+        "make": "Ferrari",
+        "model": "599"
+    },
+    {
+        "make": "Lexus",
+        "model": "LFA"
+    },
+    {
+        "make": "Bugatti",
+        "model": "Chiron"
+    }
+]
+
+console.log(showroom[0]["make"])
+console.log(showroom[1]["model"])
+
+function lookUpCars(make, model, room) {
+    var carFound = false
+    for (i = 0; i < room.length; i++){
+        if ((room[i]["make"] == make) && (room[i]["model"] == model)){
+            carFound = true
+        }
+    }
+    if (carFound == true) {
+        console.log("Car does exist in this showroom.")
+    }
+    else {
+        console.log("Car does not exist in this showroom")
+    }
+}
+
+lookUpCars("Honda", "NSX", showroom)
+lookUpCars("Bugatti", "Chiron", showroom)
+lookUpCars("Bugatti", "Veyron", showroom)
+lookUpCars("Ferrari", "FXX", showroom)
