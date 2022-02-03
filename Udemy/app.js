a0b62fd20250a04ef6373fbbb79e40813973d438
TheTacticalNuke <60157364+senmayur03@users.noreply.github.com>
2022-02-03 17:21:08 +0000

    Union Types

--- Udemy/app.js
+++ Udemy/app.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+        console.log(result);
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+console.log(combine(20, 40));
+console.log(combine('alice', 'bob'));
